1bf5d8f6331e2d00bff81cf13d570a4c5a2cc616
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-01-08 23:09:48 +0300

    added a post  function related to sending  a message

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const res = require("express/lib/response")
 const app = express()
 
 const db = require("./db")
@@ -15,4 +16,17 @@ app.get("/messages", (req, res)=>{
             res.json(data)
         }
     } )
+})
+
+
+
+app.post("/messages", ()=>{
+    Message.create(req.body, (err, newMessage)=>{
+       if(err){
+           console.log("Error")
+       }else{
+           res.status(201).json(newMessage)
+           console.log(newMessage)
+       }
+    })
 })
