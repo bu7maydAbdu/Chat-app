afeb06596bd667016fd0c25d9a454cdbb50ad91f
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-01-14 03:25:45 +0300

    some changes in users schema ,
    change the name of the file from login.js to user.js

--- frontend/src/user.js
+++ frontend/src/user.js
@@ -2,7 +2,8 @@ const {Schema, model} = require("mongoose")
 
 
 const usernamechema = new Schema({
-    username: String
+    username: String,
+    email: {type:String, required=true , unique: true}
 })
 
 const Username=model("Username", usernamechema)
