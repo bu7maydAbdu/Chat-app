c6ca365c35e4db20fd0e1f7acf219d9260e8bc67
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-01-14 03:29:43 +0300

    some styling on the login  component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div>
-            <input type="text" placeholder='write your name here'/>
-            <button>Submit</button>
+        <div className='login-page'>
+
+            <form className='login-form'>
+
+            <input className='input-username' type="text" placeholder='write your name here'/>
+            <button className='submit-button'>Submit</button>
+
+            </form>
         </div>
     )
 }
