d28121f604b3638324bab62e2e2e67fb1ebb5c44
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-01-15 03:55:34 +0300

    Update Registration.js

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -6,7 +6,7 @@ export default function registration() {
              <form className='registration-form'>
                  <input className='input-username-email-password' type="email" placeholder='write your email here'/>
                  <input className='input-username-email-password' type="text" placeholder='write your name here'/>
-                 <input className='input-username-email-password' type="password" placeholder='write your name here'/>
+                 <input className='input-username-email-password' type="password" placeholder='enter your password here'/>
 
                    <button className='submit-button'>Submit</button>
 
